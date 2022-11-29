42f89240adfe8d5ccb05ddf8b02c32ea26821ab3
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2022-11-29 13:27:32 -0500

    Update 04_📉_Data_Visualization.py

--- pages/04_📉_Data_Visualization.py
+++ pages/04_📉_Data_Visualization.py
@@ -3,59 +3,52 @@ import altair as alt
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-
 data=st.session_state['data']
 b=[" "]
 b.extend(data.columns)
 if st.checkbox('histogram'):
-    a= st.selectbox('Select a variable',b)
+        a= st.selectbox('Select a variable',b)
     
-    if a!=" ":
-        hist = alt.Chart(data).mark_bar().encode(x = a,
+        if a!=" ":
+            hist = alt.Chart(data).mark_bar().encode(x = a,
                                              y = 'count()')
-        st.altair_chart(hist)
+            st.altair_chart(hist)
 if st.checkbox('Bar Chart'):
-    b=[" "]
-    b.extend(data.columns)
-    x=st.selectbox('Select x-axis',b)
-    y=st.selectbox('Select y-axis :',b)
-    hist = alt.Chart(data).mark_bar().encode(x=x,y=y)
-    if(x!=" " and y!=" "):
-        st.altair_chart(hist)
+        
+        x=st.selectbox('Select x-axis',b)
+        y=st.selectbox('Select y-axis :',b)
+        hist = alt.Chart(data).mark_bar().encode(x=x,y=y)
+        if(x!=" " and y!=" "):
+            st.altair_chart(hist)
     #st.line_chart(data[a])
     
     
 if st.checkbox('Line Chart'):
-    b=[" "]
-    b.extend(data.columns)
-    x=st.selectbox('Select x-axis',b)
-    y=st.selectbox('Select y-axis :',b)
-    hist = alt.Chart(data).mark_line().encode(x=x,y=y)
+        
+        x=st.selectbox('Select x-axis',b)
+        y=st.selectbox('Select y-axis :',b)
+        hist = alt.Chart(data).mark_line().encode(x=x,y=y)
     #st.line_chart(data[a])
-    if(x!=" " and y!=" "):
-        st.altair_chart(hist)
+        if(x!=" " and y!=" "):
+            st.altair_chart(hist)
 if st.checkbox('Area Chart'):
-    b=[" "]
-    b.extend(data.columns)
-    
-    x=st.selectbox('Select x-axis',b)
-    y=st.selectbox('Select y-axis :',b)
-    hist = alt.Chart(data).mark_area().encode(x=x,y=y)
+        x=st.selectbox('Select x-axis',b)
+        y=st.selectbox('Select y-axis :',b)
+        hist = alt.Chart(data).mark_area().encode(x=x,y=y)
     #st.line_chart(data[a])
-    if(x!=" " and y!=" "):
-        st.altair_chart(hist)
+        if(x!=" " and y!=" "):
+            st.altair_chart(hist)
 if st.checkbox('Scatter plot'):
-    x=st.selectbox('Select 1st Variable',b)
-    y=st.selectbox('Select 2nd Variable :',b)
-    hist = alt.Chart(data).mark_circle().encode(x=x,y=y)
+        x=st.selectbox('Select 1st Variable',b)
+        y=st.selectbox('Select 2nd Variable :',b)
+        hist = alt.Chart(data).mark_circle().encode(x=x,y=y)
     #st.line_chart(data[a])
-    if(x!=" " and y!=" "):
-        st.altair_chart(hist)
+        if(x!=" " and y!=" "):
+            st.altair_chart(hist)
 if st.checkbox('Heat Map'):
-    fig, ax = plt.subplots()
-    sns.heatmap(data.corr(), ax=ax)
-    st.pyplot(fig)
+        fig, ax = plt.subplots()
+        sns.heatmap(data.corr(), ax=ax)
+        st.pyplot(fig)
 
 
     
