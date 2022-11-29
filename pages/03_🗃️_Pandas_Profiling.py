90e8ba1a7308e5b4c06649521afbb173b28a1b3a
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2022-11-29 14:34:51 -0500

    Update 03_🗃️_Pandas_Profiling.py

--- pages/03_🗃️_Pandas_Profiling.py
+++ pages/03_🗃️_Pandas_Profiling.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
 from pandas_profiling import ProfileReport
 data=st.session_state['data']
-st.cache()
+@st.cache()
 def report(data):
         
     pr = ProfileReport(data, explorative=True)
