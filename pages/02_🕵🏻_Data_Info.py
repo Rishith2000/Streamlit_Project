60c63f45eea668156cbebcba3a5c33cd067149ff
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2022-12-05 14:42:05 -0500

    Update 02_🕵🏻_Data_Info.py

--- pages/02_🕵🏻_Data_Info.py
+++ pages/02_🕵🏻_Data_Info.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import altair as alt
 
+st.subheader('Data Info')
 
 data=st.session_state['data']
 @st.cache
 def head(data):
-    return data.head()
+    return data.head() 
 @st.cache
 def Tail(data):
     return data.tail()
 if(st.checkbox('Show Shape')):
-    st.write(data.shape)
+    st.write(f"Dataset contains {data.shape[0]} rows and {data.shape[1]} columns")
 if(st.checkbox('Show Summary of Dataset')):
     st.write(data.describe())
 if(st.checkbox("Show Data")):
