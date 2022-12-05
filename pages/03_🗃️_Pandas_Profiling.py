3dfa04228f082193337c81da07934d39f51c0755
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2022-12-05 14:42:28 -0500

    Update 03_🗃️_Pandas_Profiling.py

--- pages/03_🗃️_Pandas_Profiling.py
+++ pages/03_🗃️_Pandas_Profiling.py
@@ -2,8 +2,14 @@ import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
 from pandas_profiling import ProfileReport
 data=st.session_state['data']
+st.subheader('Pandas Profiling')
+
 def report(data):
         
     pr = ProfileReport(data, explorative=True)
     return pr
-st_profile_report(report(data))
+
+pr=report(data)
+#pr.to_file('Analysis.html')
+
+st_profile_report(pr)
