b2a138210dd782276904e420730f77077780dbe6
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2022-12-08 14:59:26 -0500

    Update 02_🕵🏻_Data_Info.py

--- pages/02_🕵🏻_Data_Info.py
+++ pages/02_🕵🏻_Data_Info.py
@@ -2,27 +2,30 @@ import streamlit as st
 import altair as alt
 
 st.subheader('Data Info')
+if 'data' in st.session_state:
+    st.warning('Upload a CSV file in Upload Data page')
+else:
+    data=st.session_state['data']
+    @st.cache
+    def head(data):
+        return data.head() 
+    @st.cache
+    def Tail(data):
+        return data.tail()
+    if(st.checkbox('Show Shape')):
+        st.write(f"Dataset contains {data.shape[0]} rows and {data.shape[1]} columns")
+    if(st.checkbox("Show Columns")):
+        st.write(data.columns)
+    if(st.checkbox('Show Summary of Dataset')):
+        st.write(data.describe())
+    if(st.checkbox("Show Data")):
+        r=st.radio('Select your option',['Head','Tail','All'])
+        if(r=='Head'):
+            st.dataframe(head(data))
+        elif(r=='Tail'):
+            st.dataframe(Tail(data))
+        else:
+            st.dataframe(data)
 
-data=st.session_state['data']
-@st.cache
-def head(data):
-    return data.head() 
-@st.cache
-def Tail(data):
-    return data.tail()
-if(st.checkbox('Show Shape')):
-    st.write(f"Dataset contains {data.shape[0]} rows and {data.shape[1]} columns")
-if(st.checkbox('Show Summary of Dataset')):
-    st.write(data.describe())
-if(st.checkbox("Show Data")):
-    r=st.radio('Select your option',['Head','Tail','All'])
-    if(r=='Head'):
-        st.dataframe(head(data))
-    elif(r=='Tail'):
-        st.dataframe(Tail(data))
-    else:
-       st.dataframe(data)
-if(st.checkbox("Show Columns")):
-    st.write(data.columns)
 
 
