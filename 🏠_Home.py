6bb663acf8fd1f9660f465b78e4ff3697420c27b
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2022-12-08 14:58:23 -0500

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -6,7 +6,7 @@ image = Image.open('download.jpg')
 st.header('HandBook')
 st.image(image)
 
-st.write('This application will help you in visualizing and generating pandas profiling profile of data with few clicks without coading and help you in identifying the best model for your dataset.')
-
+st.write('This application will help you in visualizing and generating pandas profiling report of data with few clicks without coding and help you in identifying the best model for your dataset.')
+st.write('Pandas profiling offers report generation for the dataset with lots of features and customizations for the report generated')
 
 
