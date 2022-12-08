52a41ba963cf2012d95b215797f02c65c992b94b
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2022-12-08 14:59:02 -0500

    Update 01_📤_Upload Data.py

--- pages/01_📤_Upload Data.py
+++ pages/01_📤_Upload Data.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 
 st.subheader('Upload Data')
-st.write('Hi, Here you can upload your **CSV file**')
-st.write('By default This app contains iris dataset')
-st.write("[Click here](https://www.kaggle.com/datasets/uciml/iris) to know more about iris dataset")
+
+st.write('Here you can upload your ***CSV file* **')
+
+st.write("[Click here](https://github.com/zlkdfb/CSV) for sample **CSV** files")
 
 
 
@@ -14,10 +15,11 @@ if uploaded_file  is not None:
     
     df=pd.read_csv(uploaded_file)
     st.success('The Dataset Uploaded successfully')
-else:
-    df=pd.read_csv('Iris.csv')
+    st.session_state['data']=df
+#else:
+#    df=pd.read_csv('iris.csv')
 
 
 
 
-st.session_state['data']=df
+
