85d35389e3a7333b6cbdbae2a134d8178d1e4206
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2022-12-08 14:59:53 -0500

    Update 03_🗃️_Pandas_Profiling.py

--- pages/03_🗃️_Pandas_Profiling.py
+++ pages/03_🗃️_Pandas_Profiling.py
@@ -3,13 +3,15 @@ from streamlit_pandas_profiling import st_profile_report
 from pandas_profiling import ProfileReport
 data=st.session_state['data']
 st.subheader('Pandas Profiling')
-
-def report(data):
+if 'data' in st.session_state:
+    st.warning('Upload a CSV file in Upload Data page')
+else:
+    def report(data):
         
-    pr = ProfileReport(data, explorative=True)
-    return pr
+        pr = ProfileReport(data, explorative=True)
+        return pr
 
-pr=report(data)
+    pr=report(data)
 #pr.to_file('Analysis.html')
 
-st_profile_report(pr)
+    st_profile_report(pr)
