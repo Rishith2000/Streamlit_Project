8bdd6a02233c014902aa28df6cc57434e7c5b19d
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2023-04-18 19:38:19 -0400

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -9,5 +9,13 @@ st.image(image)
 st.write('This application will help you in visualizing and generating pandas profiling report of data with few clicks without coding and help you in identifying the best model for your dataset.')
 st.write('Pandas profiling offers report generation for the dataset with lots of features and customizations for the report generated')
 
-
+page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+st.markdown(page_bg_img, unsafe_allow_html=True)
 st.markdown('<style>body{background-color: #E5E5E5;}</style>',unsafe_allow_html=True)
