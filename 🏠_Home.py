b461d312338b3889c51048bd8065295141c42908
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2023-04-18 19:41:50 -0400

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -14,7 +14,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: url("https://www.shutterstock.com/image-illustration/abstract-jpg-blue-wave-dots-background-205683097");
+        background-image: url("https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.freecodecamp.org%2Fnews%2Fcontent%2Fimages%2F2022%2F09%2Fjonatan-pie-3l3RwQdHRHg-unsplash.jpg&tbnid=dfIZQWgO5XfgyM&vet=12ahUKEwj6kdW8zbT-AhULn4kEHeVgAGkQMygfegUIARC9Ag..i&imgrefurl=https%3A%2F%2Fwww.freecodecamp.org%2Fnews%2Fhtml-background-image-how-to-add-wallpaper-images-to-your-website%2F&docid=MdcI3Z9gtYMS6M&w=1920&h=1282&q=jpg%20background&ved=2ahUKEwj6kdW8zbT-AhULn4kEHeVgAGkQMygfegUIARC9Ag");
         background-size: cover;
     }
     </style>
